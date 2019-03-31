92caa8e0325cf24f876bb5230408abd874801ce0
leo <rsnav.81@gmail.com>
2019-03-30 22:27:51 -0600

    Practica_2

---
+++ Practica_2/src/practica_2/data/ArchivoProductos.java
@@ -0,0 +1,37 @@
+package practica_2.data;
+
+import practica_2.data.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import practica_2.Templates.Producto;
+
+
+public class ArchivoProductos {
+    public void guardarProductos(ArrayList<Producto> productos){
+        try {
+            FileWriter fileWriter = new FileWriter("productos.txt", true);
+            for (int i = 0; i < productos.size(); i++) {
+                fileWriter.write(
+                        "{{" + productos.get(i).getNombre() + "}}" +
+                        "{{" + productos.get(i).getPrecio() + "}}" +
+                        "{{" + productos.get(i).getPeso() + "}}" +
+                        "{{" + productos.get(i).getVolumen() + "}}"
+                );
+                System.out.println(
+                "{{" + productos.get(i).getNombre() + "}}" +
+                "{{" + productos.get(i).getPrecio() + "}}" +
+                "{{" + productos.get(i).getPeso() + "}}" +
+                "{{" + productos.get(i).getVolumen() + "}}");
+            }
+            fileWriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ArchivoProductos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
